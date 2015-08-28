becf15c1a11659ed8f7ebe5f26419e2eb288ad35
Jiri Tomasek <jtomasek@redhat.com>
2015-08-28 21:40:48 +0200

    LoginStore tests

---
+++ src/__tests__/stores/LoginStore.tests.js
@@ -0,0 +1,64 @@
+jest.autoMockOff();
+
+jest.mock('../../js/dispatchers/AppDispatcher');
+
+const AppDispatcher = require('../../js/dispatchers/AppDispatcher');
+const LoginStore = require('../../js/stores/LoginStore');
+
+let callback;
+
+let loggedInState = {
+  token: 'token',
+  user: 'admin',
+  serviceCatalog: 'service catalog',
+  metadata: 'some metadata'
+};
+
+// mock actions
+let loginUserAction = {
+  actionType:'LOGIN_USER',
+  keystoneAccess: loggedInState
+};
+
+let logoutUserAction = {
+  actionType: 'LOGOUT_USER'
+};
+
+describe('LoginStore', () => {
+  beforeEach(() => {
+    callback = AppDispatcher.register.mock.calls[0][0];
+  });
+
+  it('registers a callback with the dispatcher', function() {
+    expect(AppDispatcher.register.mock.calls.length).toBe(1);
+  });
+
+  it('sets state to keystoneAccess data when user logs in', () => {
+    LoginStore.emitChange = jest.genMockFunction();
+    callback(loginUserAction);
+    expect(LoginStore.state).toEqual({
+      token: 'token',
+      user: 'admin',
+      serviceCatalog: 'service catalog',
+      metadata: 'some metadata'
+    });
+    expect(LoginStore.emitChange).toBeCalled();
+  });
+
+  it('cleans the state when user logs out', () => {
+    LoginStore.emitChange = jest.genMockFunction();
+    callback(logoutUserAction);
+    expect(LoginStore.state).toEqual({});
+    expect(LoginStore.emitChange).toBeCalled();
+  });
+
+  it('returns state with getState', () => {
+    LoginStore.state = loggedInState;
+    expect(LoginStore.getState()).toEqual(loggedInState);
+  });
+
+  it('provides function to determine if user is logged in', () => {
+    LoginStore.state = loggedInState;
+    expect(LoginStore.isLoggedIn()).toEqual(true);
+  });
+});
