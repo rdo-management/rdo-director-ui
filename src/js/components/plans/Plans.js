71627503cecdfd8adf17cd5c552c0c0c38075d33
Florian Fuchs <flfuchs@redhat.com>
2015-10-30 10:07:44 +0100

    [WIP] Adds components, actions, store for plan file upload.
    Change-Id: Idbb84471555b438aeef938809dc9e18064e498d7

---
+++ src/js/components/plans/Plans.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import AuthenticatedComponent from '../utils/AuthenticatedComponent';
+import { PageHeader } from '../ui/PageHeader';
+import { PlanUploadFileList } from '../plans/PlanUploadFileList';
+import { PlanUploadForm } from '../plans/PlanUploadForm';
+
+export default AuthenticatedComponent(class Plans extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      plans: []
+    };
+    //this.changeListener = this._changeListener.bind(this);
+  }
+
+  componentDidMount() {
+    //PlanUploadStore.addChangeListener(this.changeListener);
+  }
+
+  componentWillUnmount() {
+    //PlanUploadStore.removeChangeListener(this.changeListener);
+  }
+
+  _changeListener() {
+    //this.setState({});
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-sm-12">
+          <PageHeader>Plans</PageHeader>
+          <PlanUploadForm />
+          <PlanUploadFileList />
+        </div>
+      </div>
+    );
+  }
+
+});
---
+++ src/js/constants/PlanUploadConstants.js
@@ -0,0 +1,7 @@
+import keyMirror from 'keymirror';
+
+export default keyMirror({
+  ADD_FILE: null,
+  PLAN_CREATED: null,
+  SET_NAME: null
+});
