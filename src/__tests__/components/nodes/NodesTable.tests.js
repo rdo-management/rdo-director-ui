88ce97a27a11a41feeee3116ecdc10d4b1aaefef
Jiri Tomasek <jtomasek@redhat.com>
2015-11-15 09:03:10 +0100

    Tables refactor
    Added additional columns in Nodes table, refactored DataTable component to
    be much simpler and less restrictive to the component which uses it.
    Enable nodes to refresh
    
    Change-Id: I8331d0b167d32adb8ecaf08053cdd60dbc81388b

--- src/__tests__/components/nodes/NodesTable.tests.js
+++ src/__tests__/components/nodes/NodesTable.tests.js
@@ -1,8 +1,15 @@
-import * as _ from 'lodash';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 
 const NodesTable = require('../../../js/components/nodes/NodesTable');
+
+const initialState = {
+  filterString: '',
+  sortBy: '',
+  sortDir: 'asc'
+};
+
+const filterString = '1';
 
 let data = [
   { uuid: 1 },
@@ -10,22 +17,29 @@ let data = [
 ];
 
 describe('NodesTable component', () => {
-  let nodesTableVdom;
+  let nodesTableVdom, nodesTableInstance;
   beforeEach(() => {
     let shallowRenderer = TestUtils.createRenderer();
     shallowRenderer.render(<NodesTable data={data}/>);
     nodesTableVdom = shallowRenderer.getRenderOutput();
+    nodesTableInstance = shallowRenderer._instance._instance;
+  });
+
+  it('should render with initial state', () => {
+    expect(nodesTableInstance.state).toEqual(initialState);
   });
 
   it('should render DataTable and pass data', () => {
     expect(nodesTableVdom.type.name).toEqual('DataTable');
     expect(nodesTableVdom.props.data).toEqual(data);
     expect(nodesTableVdom.props.noRowsRenderer.name).toBeDefined();
-    expect(nodesTableVdom.props.children.length).toEqual(4);
+    expect(nodesTableVdom.props.children.length).toEqual(7);
   });
 
-  it('should render a link on uuid column', () => {
-    let uuidColumn = _.find(nodesTableVdom.props.children, 'props.dataKey', 'uuid');
-    expect(uuidColumn.props.cellRenderer.name).toEqual('DataTableCellLink');
+  it('should be able to filter rows', () => {
+    spyOn(nodesTableInstance, '_filterData').and.callThrough();
+    nodesTableInstance.onFilter(filterString);
+    expect(nodesTableInstance.state).toEqual({ filterString: '1', sortBy: '', sortDir: 'asc' });
+    expect(nodesTableInstance._filterData).toHaveBeenCalledWith('1', nodesTableInstance.props.data);
   });
 });
--- src/__tests__/components/nodes/RegisteredNodesTabPane.tests.js
+++ src/__tests__/components/nodes/RegisteredNodesTabPane.tests.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 
-import IronicApiService from '../../../js/services/IronicApiService';
 import RegisteredNodesTabPane from '../../../js/components/nodes/RegisteredNodesTabPane';
 
 const nodes = {
@@ -12,24 +11,18 @@ const nodes = {
 };
 
 describe('RegisteredNodesTabPane component', () => {
-  let tabPaneVdom, tabPaneInstance;
+  let tabPaneVdom;
   beforeEach(() => {
     let shallowRenderer = TestUtils.createRenderer();
     shallowRenderer.render(<RegisteredNodesTabPane nodes={nodes}/>);
     tabPaneVdom = shallowRenderer.getRenderOutput();
     /* TODO(jtomasek): replace this with shallowRenderer.getMountedInstance() when it is available
        https://github.com/facebook/react/pull/4918/files */
-    tabPaneInstance = shallowRenderer._instance._instance;
+    // tabPaneInstance = shallowRenderer._instance._instance;
   });
 
   it('should render NodesTable and pass nodes as data prop', () => {
     expect(tabPaneVdom.type.name).toEqual('NodesTable');
     expect(tabPaneVdom.props.data).toEqual(nodes.registered);
-  });
-
-  it('should issue a request to list Nodes on when mounted', () => {
-    spyOn(IronicApiService, 'getNodes');
-    tabPaneInstance.componentDidMount();
-    expect(IronicApiService.getNodes).toHaveBeenCalled();
   });
 });
