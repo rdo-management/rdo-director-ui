a37f227e5e33602dee1a3e3211526f3705a19880
Jiri Tomasek <jtomasek@redhat.com>
2015-11-15 09:03:10 +0100

    Tables refactor
    Added additional columns in Nodes table, refactored DataTable component to
    be much simpler and less restrictive to the component which uses it.
    
    Change-Id: I8331d0b167d32adb8ecaf08053cdd60dbc81388b

--- src/js/components/nodes/NodesTable.js
+++ src/js/components/nodes/NodesTable.js
@@ -1,14 +1,25 @@
+import * as _ from 'lodash';
 import React from 'react';
 
 import DataTable from '../ui/tables/DataTable';
-import { DataTableCellLink } from '../ui/tables/DataTableCells';
-import { DataTableColumn, SortableDataTableColumn } from '../ui/tables/DataTableColumns';
+import { DataTableDataFieldCell,
+         DataTableHeaderCell } from '../ui/tables/DataTableCells';
+import DataTableColumn from '../ui/tables/DataTableColumn';
 
 export default class NodesTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filterString: '',
+      sortBy: '',
+      sortDir: 'asc'
+    };
+  }
+
   renderNoNodesFound() {
     return (
       <tr>
-        <td colSpan="4">
+        <td colSpan="7">
           <p></p>
           <p className="text-center">There are no Nodes available</p>
         </td>
@@ -16,25 +27,62 @@ export default class NodesTable extends React.Component {
     );
   }
 
+  onFilter(filterString) {
+    this.setState({
+      filterString: filterString
+    });
+  }
+
+  _filterData(filterString, data) {
+    let dataKeys = ['uuid'];
+    return filterString ? data.filter((row) => {
+      let result = dataKeys.filter((dataKey) => {
+        return _.includes(row[dataKey], filterString.toLowerCase());
+      });
+      return result.length > 0;
+    }) : data;
+  }
+
   render() {
+    let filteredData = this._filterData(this.state.filterString, this.props.data);
     return (
-      <DataTable {...this.props} noRowsRenderer={this.renderNoNodesFound.bind(this)}>
-        <SortableDataTableColumn dataKey="uuid"
-                                 key="uuid"
-                                 label="UUID"
-                                 cellRenderer={DataTableCellLink}
-                                 filterable/>
-        <DataTableColumn dataKey="instance_uuid"
-                         key="instance_uuid"
-                         label="Instance UUID"/>
-        <SortableDataTableColumn dataKey="power_state"
-                         key="power_state"
-                         label="Power State"
-                         filterable/>
-        <DataTableColumn dataKey="provision_state"
-                         key="provision_state"
-                         label="State"/>
+      <DataTable {...this.props}
+        rowsCount={filteredData.length}
+        noRowsRenderer={this.renderNoNodesFound.bind(this)}
+        onFilter={this.onFilter.bind(this)}
+        filterString={this.state.filterString}>
+        <DataTableColumn
+          key="uuid"
+          header={<DataTableHeaderCell key="uuid">UUID</DataTableHeaderCell>}
+          cell={<DataTableDataFieldCell data={filteredData} field="uuid"/>}/>
+        <DataTableColumn
+          key="properties.cpu_arch"
+          header={<DataTableHeaderCell key="properties.cpu_arch">CPU Arch.</DataTableHeaderCell>}
+          cell={<DataTableDataFieldCell data={filteredData} field="properties.cpu_arch"/>}/>
+        <DataTableColumn
+          key="properties.cpus"
+          header={<DataTableHeaderCell key="properties.cpus">CPU (cores)</DataTableHeaderCell>}
+          cell={<DataTableDataFieldCell data={filteredData} field="properties.cpus"/>}/>
+        <DataTableColumn
+          key="properties.local_gb"
+          header={<DataTableHeaderCell key="properties.local_gb">Disk (GB)</DataTableHeaderCell>}
+          cell={<DataTableDataFieldCell data={filteredData} field="properties.local_gb"/>}/>
+        <DataTableColumn
+          key="properties.memory_mb"
+          header={<DataTableHeaderCell key="properties.memory_mb">Memory (MB)</DataTableHeaderCell>}
+          cell={<DataTableDataFieldCell data={filteredData} field="properties.memory_mb"/>}/>
+        <DataTableColumn
+          key="power_state"
+          header={<DataTableHeaderCell key="power_state">Power State</DataTableHeaderCell>}
+          cell={<DataTableDataFieldCell data={filteredData} field="power_state"/>}/>
+        <DataTableColumn
+          key="provision_state"
+          header={<DataTableHeaderCell key="provision_state">Provision State</DataTableHeaderCell>}
+          cell={<DataTableDataFieldCell data={filteredData} field="provision_state"/>}/>
       </DataTable>
     );
   }
 }
+NodesTable.propTypes = {
+  data: React.PropTypes.array.isRequired
+};
--- src/js/components/ui/tables/DataTableRow.js
+++ src/js/components/ui/tables/DataTableRow.js
@@ -3,20 +3,14 @@ import React from 'react';
 export default class DataTableRow extends React.Component {
   render() {
     let cells = this.props.columns.map((column, index) => {
-      let CellRenderer = column.props.cellRenderer;
-      return (
-        <CellRenderer key={index} dataKey={column.props.dataKey} rowData={this.props.rowData}/>
-      );
+      return React.cloneElement(column.props.cell, { rowIndex: this.props.index, key: index });
     });
-
     return (
-      <tr>
-        {cells}
-      </tr>
+      <tr>{cells}</tr>
     );
   }
 }
 DataTableRow.propTypes = {
   columns: React.PropTypes.array.isRequired,
-  rowData: React.PropTypes.object.isRequired
+  index: React.PropTypes.number.isRequired
 };
