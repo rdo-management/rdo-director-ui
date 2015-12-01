5479b9c3bedd0c4d88970cdef8b2d93f27aa84ef
Jiri Tomasek <jtomasek@redhat.com>
2015-12-01 15:32:14 +0100

    Plans table to use DataTable
    Change-Id: I7acab5707e9f74fe7136654e7adc332c5f9a8916

--- src/js/components/plan/ListPlans.js
+++ src/js/components/plan/ListPlans.js
@@ -1,6 +1,9 @@
+import { Link } from 'react-router';
 import React from 'react';
 
-import { Link } from 'react-router';
+import DataTable from '../ui/tables/DataTable';
+import { DataTableCell, DataTableHeaderCell } from '../ui/tables/DataTableCells';
+import DataTableColumn from '../ui/tables/DataTableColumn';
 import PlansActions from '../../actions/PlansActions';
 import PlansStore from '../../stores/PlansStore';
 
@@ -29,6 +32,85 @@ export default class ListPlans extends React.Component {
     this.setState({ plans: PlansStore.getPlans() });
   }
 
+  renderNoPlans() {
+    return (
+      <tr>
+        <td colSpan="2">
+          <p></p>
+          <p className="text-center">
+            There are currently no Plans
+          </p>
+          <p className="text-center">
+            <Link to="/plans/new"
+                  query={{tab: 'newPlan'}}
+                  className="btn btn-success">Create New Plan</Link>
+          </p>
+        </td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-sm-12">
+          <Link to="/plans/new"
+                query={{tab: 'newPlan'}}
+                className="btn btn-lg btn-success">Create New Plan</Link>
+        </div>
+        <div className="col-sm-12">
+          <h2>Plans</h2>
+          <DataTable data={this.state.plans}
+                     rowsCount={this.state.plans.length}
+                     noRowsRenderer={this.renderNoPlans.bind(this)}>
+            <DataTableColumn
+              key="name"
+              header={<DataTableHeaderCell key="name">Name</DataTableHeaderCell>}
+              cell={<PlanNameCell data={this.state.plans}/>}/>
+
+            <DataTableColumn
+              key="actions"
+              header={<DataTableHeaderCell key="actions">Actions</DataTableHeaderCell>}
+              cell={<RowActionsCell className="text-right" data={this.state.plans}/>}/>
+
+          </DataTable>
+        </div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+ListPlans.propTypes = {
+  children: React.PropTypes.node
+};
+
+class RowActionsCell extends React.Component {
+  render() {
+    let plan = this.props.data[this.props.rowIndex];
+
+    if(plan.transition) {
+      // TODO(jtomasek): this causes DOMNesting validation error which should eventually go away
+      // in future React versions. See https://github.com/facebook/react/issues/5506
+      return null;
+    } else {
+      return (
+        <DataTableCell {...this.props}>
+          <Link to={`/plans/${plan.name}/edit`}
+                className="btn btn-xs btn-default">Edit</Link>
+          &nbsp;
+          <Link to={`/plans/${plan.name}/delete`}
+                className="btn btn-xs btn-danger">Delete</Link>
+        </DataTableCell>
+      );
+    }
+  }
+}
+RowActionsCell.propTypes = {
+  data: React.PropTypes.array.isRequired,
+  rowIndex: React.PropTypes.number
+};
+
+export class PlanNameCell extends React.Component {
   onPlanClick(e) {
     e.preventDefault();
     PlansActions.choosePlan(e.target.textContent);
@@ -44,55 +126,24 @@ export default class ListPlans extends React.Component {
   }
 
   render() {
-    let plans = this.state.plans.map((plan, index) => {
-      return plan.transition ? (
-            <tr key={index}>
-              <td colSpan="2" className={plan.transition}>
-                <em>Deleting <strong>{plan.name}</strong>&hellip;</em>
-              </td>
-            </tr>
-        ) : ( <tr key={index}>
-          <td className={plan.transition}>{this.getActiveIcon(plan.name)} <a href=""
-                onClick={this.onPlanClick.bind(this)}>
-                  {plan.name}</a></td>
-          <td className="plan-list-actions-col">
-            <div className="btn-group" role="group">
-              <Link to={`/plans/${plan.name}/edit`}
-                    className="btn btn-xs btn-default">Edit</Link>
-              <Link to={`/plans/${plan.name}/delete`}
-                    className="btn btn-xs btn-warning">Delete</Link>
-            </div>
-          </td>
-        </tr>
-      );
-    });
+    let plan = this.props.data[this.props.rowIndex];
 
-    return (
-      <div className="row">
-        <div className="col-sm-12">
-          <Link to="/plans/new"
-                query={{tab: 'newPlan'}}
-                className="btn btn-lg btn-success">Create New Plan</Link>
-        </div>
-        <div className="col-sm-12">
-          <h2>Plans</h2>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {plans}
-            </tbody>
-          </table>
-        </div>
-        {this.props.children}
-      </div>
-    );
+    if(plan.transition) {
+      return (
+        <DataTableCell {...this.props} colSpan="2" className={plan.transition}>
+          <em>Deleting <strong>{plan.name}</strong>&hellip;</em>
+        </DataTableCell>
+      );
+    } else {
+      return (
+        <DataTableCell {...this.props}>
+          {this.getActiveIcon(plan.name)} <a href="" onClick={this.onPlanClick}>{plan.name}</a>
+        </DataTableCell>
+      );
+    }
   }
 }
-ListPlans.propTypes = {
-  children: React.PropTypes.node
+PlanNameCell.propTypes = {
+  data: React.PropTypes.array.isRequired,
+  rowIndex: React.PropTypes.number
 };
