60cbe5bf5c6428cafd4f4a2120ae981dca57434a
Jeffrey Phillips <jephilli@redhat.com>
2015-12-10 13:59:53 -0500

    Update Notification and Validation Visuals
    Change-Id: I927d3539aba26d3158c399820707e3baa38bb8bf

---
+++ src/js/components/notifications/NotificationListItem.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ClassNames from 'classnames';
+import {FormattedDate} from 'react-intl';
+
+
+export default class NotificationListItem extends React.Component {
+  render() {
+    let classes = ClassNames({
+      'unviewed': !this.props.viewed
+    });
+    let iconClass = ClassNames({
+      'pficon': true,
+      'pficon-ok': this.props.type === 'success',
+      'pficon-info': this.props.type === 'info',
+      'pficon-warning-triangle-o': this.props.type === 'warning',
+      'pficon-error-circle-o': this.props.type === 'error' || !this.props.type
+    });
+
+    let notificationDate = new Date();
+    notificationDate.setTime(this.props.timestamp);
+
+    return (
+      <tr className={this.props.viewed ? '' : 'unviewed'} role="alert">
+        <td>
+          <span className={iconClass} aria-hidden="true"/>
+          <strong>{this.props.title}</strong>
+        </td>
+        <td>
+          <span>{this.props.message}</span>
+        </td>
+        <td>
+          <FormattedDate value={notificationDate}
+                         month="numeric" day="numeric" year="numeric"
+                         hour="numeric" minute="numeric" second="numeric" />
+        </td>
+        <td>
+          <button type="button"
+                  className="btn btn-primary btn-xs pull-right"
+                  onClick={this.props.removeNotification}>
+            Dismiss
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
+NotificationListItem.propTypes = {
+  message: React.PropTypes.string.isRequired,
+  removeNotification: React.PropTypes.func,
+  timestamp: React.PropTypes.number,
+  title: React.PropTypes.string,
+  type: React.PropTypes.string,
+  viewed: React.PropTypes.bool
+};
+NotificationListItem.defaultProps = {
+  dismissable: true,
+  title: '',
+  type: 'error'
+};
