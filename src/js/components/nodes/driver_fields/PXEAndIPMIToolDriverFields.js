9239835637e7db4c3978cb9ff851d3a2528ba6cf
Jiri Tomasek <jtomasek@redhat.com>
2015-12-16 10:27:28 -0500

    [wip] Add register nodes dialog.
    Currently no backend connection to the dialog.
    Uploading from CSV is TBD.
    No manual setting of the hardware settings for nodes.
    
    Co-Authored-By: Jeffrey Phillips <jephilli@redhat.com>
    Change-Id: I9909fdbc30cc4ddb573a91977d22e470652a7beb

---
+++ src/js/components/nodes/driver_fields/PXEAndIPMIToolDriverFields.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import HorizontalInput from '../../ui/forms/HorizontalInput';
+import HorizontalSelect from '../../ui/forms/HorizontalSelect';
+import HorizontalTextarea from '../../ui/forms/HorizontalTextarea';
+
+export default class PXEAndIPMIToolDriverFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sshUserValidator = {
+      matchRegexp: /^[a-zA-Z0-9]([._](?![._])|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$/
+    };
+    this.sshUserValidationMessage =  'Username must be between 8 and 20 characters ' +
+                                     'and contain only alphanumeric characters, underscores, ' +
+                                     'and dots.';
+
+    this.ipValidator = {
+      matchRegexp: /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+    };
+    this.ipValidatorMessage = 'Please enter a valid IPv4 Address';
+
+  }
+  render() {
+    return (
+      <div>
+        <HorizontalInput name="driver_info.ipmi_address"
+                         title="IPMI IP Address"
+                         inputColumnClasses="col-sm-7"
+                         labelColumnClasses="col-sm-5"
+                         value={this.props.driver_info.ipmi_address}
+                         validations={this.ipValidator}
+                         validationError={this.ipValidatorMessage}
+                         required />
+      </div>
+    );
+  }
+}
+PXEAndIPMIToolDriverFields.propTypes = {
+  driver_info: React.PropTypes.object
+};
