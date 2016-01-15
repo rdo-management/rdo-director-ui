aa1ba61969291129a31cacba5d6f1f5cbb2813a7
Florian Fuchs <flfuchs@redhat.com>
2016-01-15 15:32:39 +0100

    Use history API
    Change-Id: Ieb16218cf6d24fd00e890fd353218918bd5fe782

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var gulp = require('gulp');
 
 var browserSync = require('browser-sync');
+var historyApiFallback = require('connect-history-api-fallback');
 var ini = require('ini');
 var less = require('gulp-less');
 // var rename = require('gulp-rename');
@@ -21,7 +22,10 @@ gulp.task('webpack-app', ['webpack-tempstorage-worker'], function() {
 gulp.task('serve', ['config-create', 'webpack-app', 'less', 'fonts', 'images'], function(){
   browserSync.init({
     open: false,
-    server: './dist'
+    server: {
+      baseDir: './dist',
+      middleware: [ historyApiFallback() ]
+    }
   });
 
   gulp.watch('src/less/**/*.less', ['less']);
