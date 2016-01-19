8201c50c3e3b9890c88f8a3831fe8cab61746701
Jeffrey Phillips <jephilli@redhat.com>
2016-01-19 14:33:26 -0500

    Add Deployment Plan Page
    Simple page with no navigation or interactions other than showing the
    Environment Configuration and Parameters dialogs.
    
    Change-Id: I2c3d1502b62792a783ef64fd867678946f0c1437

---
+++ src/js/components/deployment-plan/DeploymentStep.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class DeploymentStep extends React.Component {
+
+  render() {
+    return (
+      <li className="deployment-step">
+        <h3>
+          <span className="deployment-step-title">{this.props.title}</span>
+        </h3>
+        <div className="container-fluid deployment-step-content">
+          <div className="row">
+            <div className="col-sm-12">
+              <div>
+                <span className="deployment-step-subtitle">{this.props.subTitle}</span>
+                {this.props.links}
+              </div>
+            </div>
+            <div className="col-sm-12">
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
+
+DeploymentStep.propTypes = {
+  children: React.PropTypes.node,
+  links: React.PropTypes.array,
+  subTitle: React.PropTypes.string,
+  title: React.PropTypes.string.isRequired
+};
