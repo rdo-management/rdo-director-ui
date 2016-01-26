8c437c8c723038af1c2953aebe8ab0a09cee5843
Jiri Tomasek <jtomasek@redhat.com>
2016-01-26 16:54:53 +0100

    Add Mistral API, Nodes Introspection workflow
    The mistral API service has been added which supports executing
    either Mistral workflow or action
    
    Added Introspect nodes action to Registered Nodes table
    When the workflow is running, poll for it to update table and
    monitor the workflow state.
    
    Change-Id: I0c1cc05c266ba60e8b9559a78b71cb8bc6ebbe02

--- src/js/components/nodes/IntrospectedNodesTabPane.js
+++ src/js/components/nodes/IntrospectedNodesTabPane.js
@@ -6,7 +6,8 @@ import NodesTable from './NodesTable';
 export default class IntrospectedNodesTabPane extends React.Component {
   render() {
     return (
-      <NodesTable data={this.props.nodes.get('introspected')}/>
+      <NodesTable data={this.props.nodes.get('introspected')}
+                  dataOperationInProgress={this.props.nodes.get('dataOperationInProgress')}/>
     );
   }
 }
