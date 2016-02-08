baf71bf74f710e3a9ad67e78a0759f0545be6339
Florian Fuchs <flfuchs@redhat.com>
2016-02-08 15:29:38 +0100

    [WIP]Ports Plans from flux to redux.
    Change-Id: I951bf172be5fa09cc4d8384c0687686f6de91724

--- src/js/components/plan/Plans.js
+++ src/js/components/plan/Plans.js
@@ -1,11 +1,22 @@
+import { connect } from 'react-redux';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import React from 'react';
 
-export default class Plans extends React.Component {
+import PlansActions from '../../actions/PlansActions';
+
+class Plans extends React.Component {
+
   render() {
     return (
       <div className="row">
         <div className="col-sm-12">
-          {this.props.children}
+          {React.cloneElement(this.props.children, {
+            currentPlanName: this.props.currentPlanName,
+            conflict: this.props.conflict,
+            planData: this.props.planData,
+            plans: this.props.plans,
+            fetchPlans: this.props.fetchPlans
+          })}
         </div>
       </div>
     );
@@ -13,5 +24,29 @@ export default class Plans extends React.Component {
 }
 
 Plans.propTypes = {
-  children: React.PropTypes.node
+  children: React.PropTypes.node,
+  conflict: React.PropTypes.string,
+  currentPlanName: React.PropTypes.string,
+  fetchPlans: React.PropTypes.func,
+  planData: ImmutablePropTypes.map,
+  plans: ImmutablePropTypes.list
 };
+
+function mapStateToProps(state) {
+  return {
+    currentPlanName: state.plans.get('currentPlanName'),
+    conflict: state.plans.get('conflict'),
+    planData: state.plans.get('planData'),
+    plans: state.plans.get('all')
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchPlans: () => {
+      dispatch(PlansActions.fetchPlans());
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Plans);
