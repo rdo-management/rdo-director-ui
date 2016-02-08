adddb1afbb59810cadd1fb228138486dcaf6e878
Florian Fuchs <flfuchs@redhat.com>
2016-02-08 15:29:38 +0100

    [WIP]Ports Plans from flux to redux.
    Change-Id: I951bf172be5fa09cc4d8384c0687686f6de91724

--- src/js/actions/PlansActions.js
+++ src/js/actions/PlansActions.js
@@ -5,6 +5,67 @@ import TripleOApiService from '../services/TripleOApiService';
 import TripleOApiErrorHandler from '../services/TripleOApiErrorHandler';
 
 export default {
+  requestPlans() {
+    return {
+      type: PlansConstants.REQUEST_PLANS
+    };
+  },
+
+  receivePlans(plans) {
+    return {
+      type: PlansConstants.RECEIVE_PLANS,
+      payload: plans
+    };
+  },
+
+  fetchPlans() {
+    return (dispatch, getState) => {
+      dispatch(this.requestPlans());
+      TripleOApiService.getPlans().then(response => {
+        dispatch(this.receivePlans(response.plans));
+      }).catch(error => {
+        console.error('Error retrieving plans PlansActions.fetchPlanslist', error); //eslint-disable-line no-console
+        dispatch(this.receivePlans([]));
+        let errorHandler = new TripleOApiErrorHandler(error);
+        errorHandler.errors.forEach((error) => {
+          NotificationActions.notify(error);
+        });
+      });
+    };
+  },
+
+  requestPlan() {
+    return {
+      type: PlansConstants.REQUEST_PLAN
+    };
+  },
+
+  receivePlan(plan) {
+    return {
+      type: PlansConstants.RECEIVE_PLAN,
+      payload: plan
+    };
+  },
+
+  fetchPlan(planName) {
+    return (dispatch, getState) => {
+      dispatch(this.requestPlan());
+      TripleOApiService.getPlan(planName).then(response => {
+        dispatch(this.receivePlan(response.plan));
+      }).catch(error => {
+        console.error('Error retrieving plan PlansActions.fetchPlan', error); //eslint-disable-line no-console
+        dispatch(this.receivePlan({}));
+        let errorHandler = new TripleOApiErrorHandler(error);
+        errorHandler.errors.forEach((error) => {
+          NotificationActions.notify(error);
+        });
+      });
+    };
+  },
+
+  /*
+   * TODO(flfuchs) remove once the login patch is merged
+   */
   listPlans() {
     TripleOApiService.getPlans().then(res => {
       AppDispatcher.dispatch({
@@ -12,7 +73,6 @@ export default {
         plans: res.plans
       });
     }).catch(error => {
-      console.error('Error retrieving plans PlansActions.listPlans', error); //eslint-disable-line no-console
       let errorHandler = new TripleOApiErrorHandler(error);
       errorHandler.errors.forEach((error) => {
         NotificationActions.notify(error);
