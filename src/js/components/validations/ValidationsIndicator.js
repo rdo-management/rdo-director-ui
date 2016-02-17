5b4e599be3f7ed37aa5d13f52528b72225b43197
Jiri Tomasek <jtomasek@redhat.com>
2016-02-17 12:17:45 +0100

    [WIP] Reduxify Validations
    Change-Id: I1af49d1fbdda93c0e881ae3645b567b9a132f84c

--- src/js/components/validations/ValidationsIndicator.js
+++ src/js/components/validations/ValidationsIndicator.js
@@ -1,66 +1,50 @@
-import * as _ from 'lodash';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import React from 'react';
 
 export default class ValidationsIndicator extends React.Component {
+  getStatusBadge(statusCounts) {
+    if (statusCounts.error > 0 || statusCounts.failed > 0 || statusCounts.new > 0) {
+      let badgeClasses, statusText, statusCount;
 
-  getStatusInfo() {
-    let allValidations = [];
-    _.forEach(this.props.validationStages, function (validationStage) {
-      allValidations = allValidations.concat(validationStage.validations);
-    });
-
-    let stateCounts = _.countBy(_.pluck(allValidations, 'status'));
-    stateCounts.total = allValidations.length;
-    stateCounts.error = (stateCounts.error || 0) + (stateCounts.failed || 0);
-    stateCounts.available = (stateCounts.available || 0) + (stateCounts.new || 0);
-    stateCounts.running = stateCounts.running || 0;
-
-    return stateCounts;
-  }
-
-  getStatusBadge (statusInfo) {
-    if (statusInfo.error > 0 || statusInfo.available > 0) {
-      let badgeClasses = 'badge';
-      let statusText = '';
-      let statusCount = 0;
-
-      if (statusInfo.error > 0) {
-        badgeClasses += ' error';
+      if (statusCounts.error > 0) {
+        badgeClasses = 'badge error';
         statusText = 'Errors';
-        statusCount = statusInfo.error;
-      }
-      else {
-        badgeClasses += ' available';
+        statusCount = statusCounts.error;
+      } else if (statusCounts.failed > 0) {
+        badgeClasses = 'badge error';
+        statusText = 'Failed';
+        statusCount = statusCounts.failed;
+      } else {
+        badgeClasses = 'badge available';
         statusText = 'Available';
-        statusCount = statusInfo.available;
+        statusCount = statusCounts.new;
       }
+
       return (
         <div className="pull-right">
           <span className="indicator-category">{statusText}</span>
           <span className={badgeClasses}>{statusCount}</span>
         </div>
       );
-    }
-    else {
+    } else {
       return false;
     }
   }
 
   render() {
-    let statusInfo = this.getStatusInfo();
-
+    const statusCounts = this.props.validationsStatusCounts;
     return (
       <a onClick={this.props.onClick} className="indicator">
         <span className="indicator-title">Validations:</span>
         <span className="indicator-category">Running</span>
-        <span className="badge running">{statusInfo.running}</span>
-        {this.getStatusBadge(statusInfo)}
+        <span className="badge running">{statusCounts.running}</span>
+        {this.getStatusBadge(statusCounts)}
       </a>
     );
   }
 }
 ValidationsIndicator.propTypes = {
   onClick: React.PropTypes.func,
-  validationStages: React.PropTypes.array
+  validationStages: ImmutablePropTypes.map.isRequired,
+  validationsStatusCounts: ImmutablePropTypes.record.isRequired
 };
-
---
+++ src/js/reducers/validationsReducer.js
@@ -0,0 +1,47 @@
+import { fromJS, Map } from 'immutable';
+
+import ValidationsConstants from '../constants/ValidationsConstants';
+import { Validation, ValidationResult, ValidationStage } from '../immutableRecords';
+
+const initialState = Map({
+  isFetching: false,
+  validationStages: Map(),
+  validations: Map(),
+  validationResults: Map()
+});
+
+export default function validationsReducer(state = initialState, action) {
+  switch(action.type) {
+
+  case ValidationsConstants.REQUEST_STAGES: {
+    return state.set('isFetching', true);
+  }
+
+  case ValidationsConstants.RECEIVE_STAGES: {
+    const validationStages = fromJS(action.payload.entities.validationStages)
+                             .map(stage => new ValidationStage(stage));
+    const validations = fromJS(action.payload.entities.validations)
+                        .map(validation => new Validation(validation));
+    const validationResults = fromJS(action.payload.entities.validationResults)
+                              .map(result => new ValidationResult(result));
+
+    return state.set('validationStages', validationStages)
+                .set('validations', validations)
+                .set('validationResults', validationResults)
+                .set('isFetching', false);
+  }
+
+  case ValidationsConstants.UPDATE_STAGE_STATUS: {
+    return state.setIn(['validationStages', action.payload.uuid, 'status'],
+                          action.payload.status);
+  }
+
+  case ValidationsConstants.UPDATE_VALIDATION_STATUS: {
+    return state.setIn(['validations', action.payload.uuid, 'status'], action.payload.status);
+  }
+
+  default:
+    return state;
+
+  }
+}
