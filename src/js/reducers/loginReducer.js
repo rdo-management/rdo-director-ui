b5bb600d2cfdc2d02ce2573f7f9695c75900cb68
Jiri Tomasek <jtomasek@redhat.com>
2016-02-24 11:53:01 +0100

    Add tests for services/utils.js
    Change-Id: I5dabb46a0334b97bde45fd99983e341adaba6ddb

--- src/js/reducers/loginReducer.js
+++ src/js/reducers/loginReducer.js
@@ -1,31 +1,21 @@
-import { Map, Record, List } from 'immutable';
-
 import LoginConstants from '../constants/LoginConstants';
+import { InitialLoginState } from '../immutableRecords/login';
 
-const InitialState = Record({
-  keystoneAccess: Map(),
-  loginForm: Map({
-    formErrors: List(),
-    formFieldErrors: Map()
-  }),
-  authInProgress: false
-});
-
-const initialState = new InitialState;
+const initialState = new InitialLoginState;
 
 export default function loginReducer(state = initialState, action) {
   switch(action.type) {
 
   case LoginConstants.USER_AUTH_STARTED:
-    return state.set('authInProgress', true);
+    return state.set('isAuthenticating', true);
 
   case LoginConstants.USER_AUTH_SUCCESS:
     return state.set('keystoneAccess', action.payload)
-                .set('authInProgress', false);
+                .set('isAuthenticating', false);
 
   case LoginConstants.USER_AUTH_FAILURE:
     return state.set('loginForm', action.payload)
-                .set('authInProgress', false);
+                .set('isAuthenticating', false);
 
   case LoginConstants.LOGOUT_USER_SUCCESS:
     return initialState;
