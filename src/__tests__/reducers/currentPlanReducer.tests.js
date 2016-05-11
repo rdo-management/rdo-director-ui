openstack-archive/tripleo-ui

Refactor Plans actions and reducer
This change splits Current plan and Stacks into separate
actions and action creators

Change-Id: I02fce1d6a3b879d265cd3f2c76899a6464f7d329

src/__tests__/reducers/currentPlanReducer.tests.js
@@ -1,0 +1,42 @@
+import matchers from 'jasmine-immutable-matchers';
+
+import { CurrentPlanState } from '../../js/immutableRecords/plans';
+import currentPlanReducer from '../../js/reducers/currentPlanReducer';
+import CurrentPlanActions from '../../js/actions/CurrentPlanActions';
+
+
+describe('plansReducer state', () => {
+
+  beforeEach(() => {
+    jasmine.addMatchers(matchers);
+  });
+
+  describe('default state', () => {
+    let state;
+
+    beforeEach(() => {
+      state = currentPlanReducer(undefined, {type: 'undefined-action'});
+    });
+
+    it('`conflict` is undefined', () => {
+      expect(state.get('conflict')).not.toBeDefined();
+    });
+
+    it('`currentPlanName` is undefined', () => {
+      expect(state.get('currentPlanName')).not.toBeDefined();
+    });
+  });
+
+  describe('PLAN_CHOSEN', () => {
+    let state;
+
+    beforeEach(() => {
+      state = currentPlanReducer(new CurrentPlanState(),
+                                 CurrentPlanActions.planChosen('another-cloud'));
+    });
+
+    it('sets the current planName', () => {
+      expect(state.get('currentPlanName')).toEqual('another-cloud');
+    });
+  });
+});

src/__tests__/selectors/stacks.tests.js
@@ -1,0 +1,79 @@
+import { Map } from 'immutable';
+import matchers from 'jasmine-immutable-matchers';
+
+import { getCurrentStackDeploymentProgress,
+         getCurrentStack } from '../../js/selectors/stacks';
+import { CurrentPlanState } from '../../js/immutableRecords/plans';
+import { Stack, StacksState } from '../../js/immutableRecords/stacks';
+
+describe('stacks selectors', () => {
+  beforeEach(() => {
+    jasmine.addMatchers(matchers);
+  });
+
+  describe('getCurrentStack()', () => {
+    const state = {
+      stacks: new StacksState({
+        stacks: Map({
+          overcloud: Stack({ stack_name: 'overcloud', stack_status: 'CREATE_COMPLETE' }),
+          anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_FAILED' })
+        })
+      }),
+      currentPlan: new CurrentPlanState({
+        currentPlanName: 'overcloud'
+      })
+    };
+
+    it('returns a stack based on the currentPlanName', () => {
+      expect(getCurrentStack(state)).toEqualImmutable(
+         Stack({ stack_name: 'overcloud', stack_status: 'CREATE_COMPLETE' })
+      );
+    });
+  });
+
+  describe('getCurrentStackDeploymentProgress', () => {
+    it('returns true if the current plan\'s deployment is in progress', () => {
+      const state = {
+        stacks: new StacksState({
+          stacks: Map({
+            overcloud: Stack({ stack_name: 'overcloud', stack_status: 'CREATE_IN_PROGRESS' }),
+            anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_FAILED' })
+          })
+        }),
+        currentPlan: new CurrentPlanState({
+          currentPlanName: 'overcloud'
+        })
+      };
+      expect(getCurrentStackDeploymentProgress(state)).toBe(true);
+    });
+
+    it('returns false if the current plan\'s deployment is not in progress', () => {
+      const state = {
+        stacks: new StacksState({
+          stacks: Map({
+            overcloud: Stack({ stack_name: 'overcloud', stack_status: 'CREATE_FAILED' }),
+            anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_IN_PROGRESS' })
+          })
+        }),
+        currentPlan: new CurrentPlanState({
+          currentplanname: 'overcloud'
+        })
+      };
+      expect(getCurrentStackDeploymentProgress(state)).toBe(false);
+    });
+
+    it('returns false if the current plan does not have an associated stack', () => {
+      const state = {
+        stacks: new StacksState({
+          stacks: Map({
+            anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_IN_PROGRESS' })
+          })
+        }),
+        currentPlan: new CurrentPlanState({
+          currentplanname: 'overcloud'
+        })
+      };
+      expect(getCurrentStackDeploymentProgress(state)).toBe(false);
+    });
+  });
+});

src/js/reducers/currentPlanReducer.js
@@ -1,0 +1,21 @@
+import { CurrentPlanState } from '../immutableRecords/plans';
+import PlansConstants from '../constants/PlansConstants';
+
+const initialState = new CurrentPlanState;
+
+export default function currentPlanReducer(state = initialState, action) {
+  switch(action.type) {
+
+  case PlansConstants.PLAN_CHOSEN:
+    return state.set('currentPlanName', action.payload);
+
+  case PlansConstants.PLAN_DETECTED:
+    return state
+            .set('currentPlanName', action.payload.currentPlanName)
+            .set('conflict', action.payload.conflict);
+
+  default:
+    return state;
+
+  }
+}

rdo-management/rdo-director-ui

src/__tests__/reducers/currentPlanReducer.tests.js
@@ -1,0 +1,42 @@
+import matchers from 'jasmine-immutable-matchers';
+
+import { CurrentPlanState } from '../../js/immutableRecords/currentPlan';
+import currentPlanReducer from '../../js/reducers/currentPlanReducer';
+import CurrentPlanActions from '../../js/actions/CurrentPlanActions';
+
+
+describe('plansReducer state', () => {
+
+  beforeEach(() => {
+    jasmine.addMatchers(matchers);
+  });
+
+  describe('default state', () => {
+    let state;
+
+    beforeEach(() => {
+      state = currentPlanReducer(undefined, {type: 'undefined-action'});
+    });
+
+    it('`conflict` is undefined', () => {
+      expect(state.get('conflict')).not.toBeDefined();
+    });
+
+    it('`currentPlanName` is undefined', () => {
+      expect(state.get('currentPlanName')).not.toBeDefined();
+    });
+  });
+
+  describe('PLAN_CHOSEN', () => {
+    let state;
+
+    beforeEach(() => {
+      state = currentPlanReducer(new CurrentPlanState(),
+                                 CurrentPlanActions.planChosen('another-cloud'));
+    });
+
+    it('sets the current planName', () => {
+      expect(state.get('currentPlanName')).toEqual('another-cloud');
+    });
+  });
+});

src/__tests__/selectors/stacks.tests.js
@@ -1,0 +1,79 @@
+import { Map } from 'immutable';
+import matchers from 'jasmine-immutable-matchers';
+
+import { getCurrentStackDeploymentProgress,
+         getCurrentStack } from '../../js/selectors/stacks';
+import { CurrentPlanState } from '../../js/immutableRecords/currentPlan';
+import { Stack, StacksState } from '../../js/immutableRecords/stacks';
+
+describe('stacks selectors', () => {
+  beforeEach(() => {
+    jasmine.addMatchers(matchers);
+  });
+
+  describe('getCurrentStack()', () => {
+    const state = {
+      stacks: new StacksState({
+        stacks: Map({
+          overcloud: Stack({ stack_name: 'overcloud', stack_status: 'CREATE_COMPLETE' }),
+          anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_FAILED' })
+        })
+      }),
+      currentPlan: new CurrentPlanState({
+        currentPlanName: 'overcloud'
+      })
+    };
+
+    it('returns a stack based on the currentPlanName', () => {
+      expect(getCurrentStack(state)).toEqualImmutable(
+         Stack({ stack_name: 'overcloud', stack_status: 'CREATE_COMPLETE' })
+      );
+    });
+  });
+
+  describe('getCurrentStackDeploymentProgress', () => {
+    it('returns true if the current plan\'s deployment is in progress', () => {
+      const state = {
+        stacks: new StacksState({
+          stacks: Map({
+            overcloud: Stack({ stack_name: 'overcloud', stack_status: 'CREATE_IN_PROGRESS' }),
+            anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_FAILED' })
+          })
+        }),
+        currentPlan: new CurrentPlanState({
+          currentPlanName: 'overcloud'
+        })
+      };
+      expect(getCurrentStackDeploymentProgress(state)).toBe(true);
+    });
+
+    it('returns false if the current plan\'s deployment is not in progress', () => {
+      const state = {
+        stacks: new StacksState({
+          stacks: Map({
+            overcloud: Stack({ stack_name: 'overcloud', stack_status: 'CREATE_FAILED' }),
+            anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_IN_PROGRESS' })
+          })
+        }),
+        currentPlan: new CurrentPlanState({
+          currentplanname: 'overcloud'
+        })
+      };
+      expect(getCurrentStackDeploymentProgress(state)).toBe(false);
+    });
+
+    it('returns false if the current plan does not have an associated stack', () => {
+      const state = {
+        stacks: new StacksState({
+          stacks: Map({
+            anothercloud: Stack({ stack_name: 'anothercloud', stack_status: 'CREATE_IN_PROGRESS' })
+          })
+        }),
+        currentPlan: new CurrentPlanState({
+          currentplanname: 'overcloud'
+        })
+      };
+      expect(getCurrentStackDeploymentProgress(state)).toBe(false);
+    });
+  });
+});

src/js/reducers/currentPlanReducer.js
@@ -1,0 +1,21 @@
+import { CurrentPlanState } from '../immutableRecords/currentPlan';
+import PlansConstants from '../constants/PlansConstants';
+
+const initialState = new CurrentPlanState;
+
+export default function currentPlanReducer(state = initialState, action) {
+  switch(action.type) {
+
+  case PlansConstants.PLAN_CHOSEN:
+    return state.set('currentPlanName', action.payload);
+
+  case PlansConstants.PLAN_DETECTED:
+    return state
+            .set('currentPlanName', action.payload.currentPlanName)
+            .set('conflict', action.payload.conflict);
+
+  default:
+    return state;
+
+  }
+}